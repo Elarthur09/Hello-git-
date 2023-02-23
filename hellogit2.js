c376bb5f06257072643792c616c6e9331aa5a6df
Elarthur09 <arturozarco391@gmail.com>
2023-02-23 13:17:55 -0600

    Inserte una clase persona

--- hellogit2.js
+++ hellogit2.js
@@ -1,1 +1,28 @@
 console.log('Hello git')
+
+class Personas{
+    //Create construct
+    constructor({
+        nombre,
+        Lastname,
+        age,
+        Alias = []
+    })
+    // save and referenciamos
+    {
+        this.nombre = nombre;
+        this.Lastname =Lastname;
+        this.age = age;
+        this.Alias = Alias;
+    }
+}
+
+//Instanciamos
+const Arturo = new Personas({
+    nombre:'Arturo',
+    Lastname: 'Israel',
+    age : 23,
+    Alias : 'EL pro'
+})
+
+console.log(Arturo)
